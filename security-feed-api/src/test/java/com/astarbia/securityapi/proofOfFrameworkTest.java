1e73fe6afa60d4d71b3cf10486d095d348f9173a
Mike Astarb <mike@astarbia.com>
2022-01-29 20:32:08 -0500

    test: adds unit test framework and updates e2e config
    The e2e test configuration was setup in such a way that the e2e test
    suite would fail the build if it did not run with 100% passing. This is
    not always what we want, or may not be the right point in time to make
    that go/no-go decision

---
+++ security-feed-api/src/test/java/com/astarbia/securityapi/proofOfFrameworkTest.java
@@ -0,0 +1,12 @@
+package com.astarbia.securityapi;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+public class proofOfFrameworkTest {
+
+    @Test
+    public void passingUnitTest() {
+        Assert.assertTrue(true);
+    }
+}
