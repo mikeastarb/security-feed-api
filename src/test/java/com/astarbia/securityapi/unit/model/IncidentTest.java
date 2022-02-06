cc384bdf4cb5265ff8e09ea161e5f07abf32e208
Mike Astarb <mike@astarbia.com>
2022-02-05 20:03:21 -0500

    test: adds tests to ensure attempts to set bad lat/long don't overwrite valid values

--- src/test/java/com/astarbia/securityapi/unit/model/IncidentTest.java
+++ src/test/java/com/astarbia/securityapi/unit/model/IncidentTest.java
@@ -2,6 +2,7 @@ package com.astarbia.securityapi.unit.model;
 
 import com.astarbia.securityapi.exception.RangeOutOfBoundsException;
 import com.astarbia.securityapi.model.Incident;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Value;
@@ -43,6 +44,34 @@ public class IncidentTest {
         assertThrows(RangeOutOfBoundsException.class, () -> {
             incident.setLongitude(longitude);
         });
+    }
+
+    @Test
+    public void settingInvalidLatitudeDoesNotOverwriteValue() {
+        Incident incident = getTestIncident();
+
+        try {
+            incident.setLatitude(70);
+            incident.setLatitude(-185);
+        } catch (RangeOutOfBoundsException e) {
+            // Expected to catch on the second set; not doing anything about it
+        }
+
+        assertThat(incident.getLatitude()).isEqualTo(70);
+    }
+
+    @Test
+    public void settingInvalidLongitudeDoesNotOverwriteValue() {
+        Incident incident = getTestIncident();
+
+        try {
+            incident.setLongitude(70);
+            incident.setLongitude(-185);
+        } catch (RangeOutOfBoundsException e) {
+            // Expected to catch on the second set; not doing anything about it
+        }
+
+        assertThat(incident.getLongitude()).isEqualTo(70);
     }
 
     private Incident getTestIncident() {
