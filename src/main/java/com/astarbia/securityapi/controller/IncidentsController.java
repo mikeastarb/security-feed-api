c6aebcf2afbb5466b6073c59416ee07825c771ba
Mike Astarb <mike@astarbia.com>
2022-02-05 20:21:56 -0500

    feat: adds post mapping for incidents endpoint

--- src/main/java/com/astarbia/securityapi/controller/IncidentsController.java
+++ src/main/java/com/astarbia/securityapi/controller/IncidentsController.java
@@ -2,6 +2,7 @@ package com.astarbia.securityapi.controller;
 
 import com.astarbia.securityapi.model.response.IncidentListResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class IncidentsController {
     @GetMapping(value = "/incidents", produces= { "application/json" })
     public IncidentListResponse getAllIncidents() {
         return new IncidentListResponse();
+    }
+
+    @PostMapping(value = "/incidents", produces = { "application/json" })
+    public void addNewIncident() {
+        
     }
 }
